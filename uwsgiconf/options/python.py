404c9755af216af8071337afbb4e38b87643973a
idle sign <idlesign@yandex.ru>
2017-08-09 20:22:47 +0700

    Shared import hint.

--- uwsgiconf/options/python.py
+++ uwsgiconf/options/python.py
@@ -171,6 +171,7 @@ class Python(OptionsGroup):
         :param list|str|unicode modules:
 
         :param bool shared: Import a python module in all of the processes.
+            This is done after fork but before request processing.
 
         :param bool into_spooler: Import a python module in the spooler.
             http://uwsgi-docs.readthedocs.io/en/latest/Spooler.html
