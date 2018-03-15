2e6242c20d4599bced30f0e6fba3d32958d27fd8
Igor Starikov <idlesign@yandex.ru>
2018-03-15 15:08:24 +0700

    Test added

--- tests/runtime/test_runtime_scheduling.py
+++ tests/runtime/test_runtime_scheduling.py
@@ -1,3 +1,5 @@
+import pytest
+
 from uwsgiconf.runtime.scheduling import *
 
 
@@ -17,3 +19,24 @@ def test_cron():
     @register_cron(hour=-3)
     def fire1():
         pass
+
+    with pytest.raises(RuntimeConfigurationError):
+        register_cron(hour='-%s/2')
+
+    now = datetime.now()
+
+    @register_cron(hour='%s-%s/2' % (now.hour, now.hour+3), weekday='0-6')
+    def runnable1():
+        return 100
+
+    @register_cron(hour='%s-%s' % (now.hour, now.hour+3))
+    def runnable2():
+        return 200
+
+    @register_cron(hour='%s-%s' % (now.hour-3, now.hour-1))
+    def not_runnable():
+        return 200
+
+    assert runnable1() == 100
+    assert runnable2() == 200
+    assert not_runnable() is None
