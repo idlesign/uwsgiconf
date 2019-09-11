a4330ab65475a7196c053aba1acd4039c3aed90c
idle sign <idlesign@yandex.ru>
2019-09-11 21:14:32 +0700

    Fixed is_stub attribute binding to uwsgi/uwsgi_stub modules

--- uwsgiconf/uwsgi.py
+++ uwsgiconf/uwsgi.py
@@ -8,7 +8,7 @@ from .exceptions import UwsgiconfException as __DummyException
 
 
 if False:  # pragma: nocover
-    # Give IDEs a chance load stub symbols.
+    # Give IDEs a chance to load stub symbols.
     from .uwsgi_stub import *
 
 
@@ -16,23 +16,19 @@ try:  # pragma: nocover
     if __env.get(ENV_FORCE_STUB, False):
         raise __DummyException('`UWSGI_FORCE_STUB` is found in env.')
 
-    is_stub = False  # type: bool
-    """Indicates whether stub is used instead of real `uwsgi` module."""
-
-    ####################################################################
-
     import uwsgi
+
+    uwsgi.is_stub = False  # type: bool
+    """Indicates whether stub is used instead of real `uwsgi` module."""
 
     # The following allows proper dynamic attributes (e.g. ``env``) addressing.
     modules[__name__] = uwsgi
 
 except (ImportError, __DummyException):
 
-    is_stub = True  # type: bool
-    """Indicates whether stub is used instead of real `uwsgi` module."""
-
-    ####################################################################
-
     from . import uwsgi_stub
+
+    uwsgi_stub.is_stub = True  # type: bool
+    """Indicates whether stub is used instead of real `uwsgi` module."""
 
     modules[__name__] = uwsgi_stub
