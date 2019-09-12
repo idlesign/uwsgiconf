a046298313cddd9443620d0bdbc044d68c2dc529
idle sign <idlesign@yandex.ru>
2019-09-12 19:40:30 +0700

    Extend test suite.

---
+++ tests/runtime/test_runtime_platform.py
@@ -0,0 +1,12 @@
+from uwsgiconf.runtime.platform import uwsgi
+
+
+def test_platform_stub():
+
+    assert not uwsgi.worker_id
+    assert not uwsgi.workers_info
+    assert not uwsgi.ready_for_requests
+    assert uwsgi.memory == (0, 0)
+    assert not uwsgi.clock
+    assert not uwsgi.get_listen_queue()
+    assert not uwsgi.get_version()
