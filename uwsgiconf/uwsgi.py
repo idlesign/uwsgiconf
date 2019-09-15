6abe21f9d803d2cf47aa118c7d2423fa3ea27400
idle sign <idlesign@yandex.ru>
2019-09-15 10:27:41 +0700

    Hide unwanted stuff from 'uwsgi' submodule.

--- uwsgiconf/uwsgi.py
+++ uwsgiconf/uwsgi.py
@@ -1,19 +1,18 @@
 from __future__ import absolute_import
 
-from sys import modules
 from os import environ as __env
+from sys import modules as __modules
 
-from .settings import ENV_FORCE_STUB
 from .exceptions import UwsgiconfException as __DummyException
-
+from .settings import ENV_FORCE_STUB as __ENV_FORCE_STUB
 
 if False:  # pragma: nocover
     # Give IDEs a chance to load stub symbols.
-    from .uwsgi_stub import *
+    pass
 
 
 try:  # pragma: nocover
-    if __env.get(ENV_FORCE_STUB, False):
+    if __env.get(__ENV_FORCE_STUB, False):
         raise __DummyException('`UWSGI_FORCE_STUB` is found in env.')
 
     import uwsgi
@@ -22,13 +21,9 @@ try:  # pragma: nocover
     """Indicates whether stub is used instead of real `uwsgi` module."""
 
     # The following allows proper dynamic attributes (e.g. ``env``) addressing.
-    modules[__name__] = uwsgi
+    __modules[__name__] = uwsgi
 
 except (ImportError, __DummyException):
 
     from . import uwsgi_stub
-
-    uwsgi_stub.is_stub = True  # type: bool
-    """Indicates whether stub is used instead of real `uwsgi` module."""
-
-    modules[__name__] = uwsgi_stub
+    __modules[__name__] = uwsgi_stub
