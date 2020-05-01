5bbbcc9a1daabdaa03efca9ebb4d5504db41ccc2
idle sign <idlesign@yandex.ru>
2020-05-01 20:37:28 +0700

    Removed deprecated runtime.async module

--- uwsgiconf/runtime/async.py
+++
@@ -1,7 +0,0 @@
-# This module is for backward compatibility:
-# since in python 3.7+ `async` is a keyword it cannot be used in imports.
-from warnings import warn
-
-from .asynced import *
-
-warn('runtime.async module is deprecated. Please use runtime.asynced', DeprecationWarning)
