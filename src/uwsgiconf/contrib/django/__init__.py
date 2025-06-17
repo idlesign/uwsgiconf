2befe5f7905801ac40ac2645374bfceece30c9bc
Igor Starikov <idlesign@yandex.ru>
2025-06-17 14:17:08 +0700

    Fix compat with `pkg_resources` hooks.

--- src/uwsgiconf/contrib/django/__init__.py
+++ src/uwsgiconf/contrib/django/__init__.py
@@ -1,5 +1,5 @@
 import sys
 from pathlib import Path
 
-# To support 'uwsgiconf.contrib.django.uwsgify' in INSTALLED_APPS:
-sys.path.insert(0, Path(__file__).parent)
+# To support 'uwsgiconf.contrib.django.uwsgify' in INSTALLED_APPS (old Python):
+sys.path.insert(0, f'{Path(__file__).parent}')
